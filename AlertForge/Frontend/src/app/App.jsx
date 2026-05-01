ba8af2db1dbcdc5c1b03c246ce88719ba3c1b5ad
Soumipal56 <soumiisc2020@gmail.com>
2026-05-01 09:15:55 +0530

    feat: clerk frontend done

--- AlertForge/Frontend/src/app/App.jsx
+++ AlertForge/Frontend/src/app/App.jsx
@@ -1,5 +1,6 @@
 import { routes } from "./app.routes";
 import { RouterProvider } from "react-router";
+import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
 
 function App() {
   return (
