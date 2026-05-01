e77e34e2c21df8eb8ab85f36b5626b5352591cb0
Mustafa Ali <mustafaramakda61@gmail.com>
2026-05-01 19:08:27 +0300

    feat(auth): add login and registration routes, update Navbar for user authentication
    - Introduced /login and /register routes with corresponding components
    - Updated Navbar to conditionally render authentication buttons based on user sign-in status
    - Enhanced user navigation experience with direct links to login and registration

--- AlertForge/Frontend/src/app/app.routes.jsx
+++ AlertForge/Frontend/src/app/app.routes.jsx
@@ -2,9 +2,10 @@ import { createBrowserRouter } from "react-router";
 import Landing from "@/features/landing/Landing";
 import WarRoom from "@/pages/WarRoom";
 import WarRoomChat from "@/pages/WarRoomChat";
+import LoginPage from "@/features/pages/auth/Login";
+import RegisterPage from "@/features/pages/auth/Register";
 
 export const routes = createBrowserRouter([
-
   {
     path: "/create-incident",
     element: <WarRoom />,
@@ -16,5 +17,13 @@ export const routes = createBrowserRouter([
   {
     path: "/",
     element: <Landing />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
   },
 ]);
